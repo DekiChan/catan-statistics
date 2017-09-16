dd7bb89cffa24e9e1bcb3fb4751053058483330b
dekichan <dejan.rabic@gmail.com>
2017-09-16 22:55:35 +0200

    Fix DiceRollFactory to use 6-sided dice.

--- database/factories/DiceRollFactory.php
+++ database/factories/DiceRollFactory.php
@@ -19,8 +19,12 @@ $factory->define(App\DiceRoll::class, function (Faker $faker) {
         ];
     }
 
+    // rand(0, 12) was wrong, because we have two 6-sided
+    // dice and not one 12-sided
+    $value = rand(1, 6) + rand(1, 6);
+
     return [
-        'value' => rand(2, 12),
+        'value' => $value,
         'color' => $colors[rand(0, count($colors) - 1)],
         'game_id' => $game->id,
     ];
